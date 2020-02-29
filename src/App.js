cbdc89ac79265ad94b8ffa1e33d0529c59c78220
Kwon770 <sckwon770@gmail.com>
2020-02-29 12:20:42 +0900

    2 Hello World with Styled Components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
-import React, { Component, Fragment } from "react";
-import "./App.css";
+import React, { Component } from "react";
+import styled from "styled-components";
 import { render } from "@testing-library/react";
 
 class App extends Component {
   render() {
     return (
-      <Fragment>
-        <button className="button button-success">Hello</button>
-        <button className="button button-danger">Hello</button>
-      </Fragment>
+      <Container>
+        <Button success>Hello</Button>
+        <Button danger>Hello</Button>
+      </Container>
     );
   }
 }
+
+// <styled-components>.<html tag>` css `
+const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  background-color: #34495e;
+`;
+
+const Button = styled.button`
+  border-radius: 50px;
+  padding: 5px;
+  min-width: 120px;
+  color: white;
+  font-weight: 600;
+  appearance: none;
+  cursor: pointer;
+
+  &:active,
+  &:focus {
+    outline: none;
+  }
+  background-color: ${props => (props.danger ? "#e74c3c" : "#2ecc71")};
+`;
 
 export default App;
