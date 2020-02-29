0e61e7d1cfd3171e173cf1f1999dd841c1bf180e
Kwon770 <sckwon770@gmail.com>
2020-02-29 15:08:40 +0900

    8 CSS on React Native

---
+++ styled-native/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import styled from "styled-components";
+
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  padding: 10px 20px;
+`;
+
+const Title = styled.Text`
+  font-weight: 600;
+  font-size: 32px;
+`;
+
+export default function App() {
+  return (
+    <Container>
+      <Title>Open up App.js to start working on your app!</Title>
+    </Container>
+  );
+}
